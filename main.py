68cb034847a3ad2ba0040bdccef1c28cf2bec6da
Dor Haim Shmuel <shmueldo@post.bgu.ac.il>
2022-10-23 19:19:21 +0300

    Code: organization + documentation

--- main.py
+++ main.py
@@ -1,4 +1,3 @@
-from pickle import FALSE
 import sys
 import torch
 import os
@@ -64,16 +63,16 @@ if __name__ == "__main__":
     scenario = "NarrowBand"
     mode = "coherent"
     
+    
     ############################
     ###   Create Data Sets   ###  
     ############################
     
     if CREATE_DATA:
-        Set_Overall_Seed(5413)
+        Set_Overall_Seed()
         Create_Training_Data = False
         Create_Testing_Data = True
         print("Creating Data...")
-        
         if Create_Training_Data:
         ## Training Datasets
             DataSet_x_train, DataSet_Rx_train, _ = CreateDataSetCombined(
@@ -102,6 +101,7 @@ if __name__ == "__main__":
     ############################
     ###    Load Data Sets    ###
     ############################
+    
     if LOAD_DATA:
         train_details_line = '_{}_{}_{}_M={}_N={}_T={}_SNR={}.h5'.format(scenario, mode, nNumberOfSampels, M, N, T, SNR)
         test_details_line = '_{}_{}_{}_M={}_N={}_T={}_SNR={}.h5'.format(scenario, mode, int(Train_Test_Ratio * nNumberOfSampels), M, N, T, SNR)
@@ -114,17 +114,18 @@ if __name__ == "__main__":
         Sys_Model        = Read_Data(Main_Data_path + Data_Scenario_path + r"\\TestData\\Sys_Model"      + test_details_line)
 
 
+    ############################
+    ###    Training stage    ###
+    ############################
+    
     if TRAIN_MODE:
-
-        ############################
-        ##   Training parameters  ##
-        ############################
+        # Training parameters
         optimal_gamma_val = 1
         optimal_bs = 2048
         optimal_lr = 0.00001
         optimal_step = 80
         epochs = 1
-        
+
         Test_losses = []
         train_loss_lists = []
         validation_loss_lists = []
@@ -135,9 +136,9 @@ if __name__ == "__main__":
         fig = plt.figure(figsize=(8, 6), dpi=80)
         
         ############################
-        ###    Run Simulations   ###
+        ###    Run Simulation    ###
         ############################
-        
+ 
         print("\n--- New Simulation ---\n")
         print("Description: Simulation of closely spaced sources with T = {}, Tau = {}, SNR = {}, {} sources".format(T, tau, SNR, mode))
         print("Simulation parameters:")
@@ -180,58 +181,64 @@ if __name__ == "__main__":
         validation_loss_lists.append(loss_valid_list)
         Test_losses.append(Test_loss)
         
+        # Plotting train & validation curves
         plt.plot(range(epochs), loss_train_list, label="tr {}".format(optimal_lr))
         plt.plot(range(epochs), loss_valid_list, label="vl {}".format(optimal_lr))
-
         plt.title("Learning Curves: Loss per Epoch - different Learning Rates")
         plt.xlabel("Epochs")
         plt.ylabel("Loss")
         plt.legend(bbox_to_anchor=(0.95,0.5), loc="center left", borderaxespad=0)
         
+        # Plots saving
         if(SAVE_TO_FILE):
             plt.savefig(Simulations_path + r"\\Results\\Plots\\" + dt_string_for_save + r".png")
         else:
             plt.show()
     
-    if EVALUATE_MODE:
-        ############################
-        ###    Load Data Sets    ###
-        ############################
+
+    ############################
+    ###   Evaluation stage   ###
+    ############################
     
+    if EVALUATE_MODE:
         RootMUSIC_loss = []
         MUSIC_loss = []
         SPS_RootMUSIC_loss = []
         SPS_MUSIC_loss = []
         DeepRootTest_loss = []
-        for SNR in [-2]:
-            test_details_line = '_{}_{}_{}_M={}_N={}_T={}_SNR={}.h5'.format(scenario, mode, int(Train_Test_Ratio * nNumberOfSampels), M, N, T, SNR)
-            # test_details_line = '_{}_{}_{}_M={}_N={}_T={}.h5'.format(scenario, mode, int(Train_Test_Ratio * nNumberOfSampels), M, N, T)
+        
+        test_details_line = '_{}_{}_{}_M={}_N={}_T={}_SNR={}.h5'.format(scenario, mode, int(Train_Test_Ratio * nNumberOfSampels), M, N, T, SNR)
+        # test_details_line = '_{}_{}_{}_M={}_N={}_T={}.h5'.format(scenario, mode, int(Train_Test_Ratio * nNumberOfSampels), M, N, T)
 
-            DataSet_Rx_test  = Read_Data(Main_Data_path + Data_Scenario_path + r"\\TestData\\DataSet_Rx"     + test_details_line)
-            DataSet_x_test   = Read_Data(Main_Data_path + Data_Scenario_path + r"\\TestData\\DataSet_x"      + test_details_line)
-            Sys_Model        = Read_Data(Main_Data_path + Data_Scenario_path + r"\\TestData\\Sys_Model"      + test_details_line)
-            
-            print("SNR = {}".format(SNR))
-            print("scenario = {}".format(scenario))
-            print("mode = {}".format(mode))
-            print("Observations = {}".format(T))
-            
-            # loading_path = saving_path + r"\Final_models" + r"\model_M={}_{}_Tau={}_SNR={}_T={}".format(M, mode, tau, SNR, T)
-            loading_path = saving_path + r"\Final_models" + r"/model_M={}_{}_Tau={}_SNR={}_T={}".format(M, mode, tau, SNR, T)
-            model = Deep_Root_Net_AntiRectifier(tau=tau, ActivationVal=0.5)  
-            # model = Deep_Root_Net(tau=tau, ActivationVal=0.5)                                         
-            
-            # Load it to the specified device, either gpu or cpu
-            model = model.to(device)         
-            if torch.cuda.is_available() == False:
-                            model.load_state_dict(torch.load(loading_path, map_location=torch.device('cpu')))
-            
-            Losses = PlotSpectrum(model, DataSet_Rx_test, DataSet_x_test, Sys_Model)
-            RootMUSIC_loss.append(Losses[0])
-            MUSIC_loss.append(Losses[1])
-            SPS_RootMUSIC_loss.append(Losses[2])
-            SPS_MUSIC_loss.append(Losses[3])
-            DeepRootTest_loss.append(Losses[4])
+        DataSet_Rx_test  = Read_Data(Main_Data_path + Data_Scenario_path + r"\\TestData\\DataSet_Rx"     + test_details_line)
+        DataSet_x_test   = Read_Data(Main_Data_path + Data_Scenario_path + r"\\TestData\\DataSet_x"      + test_details_line)
+        Sys_Model        = Read_Data(Main_Data_path + Data_Scenario_path + r"\\TestData\\Sys_Model"      + test_details_line)
+        
+        print("SNR = {}".format(SNR))
+        print("scenario = {}".format(scenario))
+        print("mode = {}".format(mode))
+        print("Observations = {}".format(T))
+        
+
+        ############################
+        ###    Load Data Set     ###
+        ############################
+        # loading_path = saving_path + r"\Final_models" + r"\model_M={}_{}_Tau={}_SNR={}_T={}".format(M, mode, tau, SNR, T)
+        loading_path = saving_path + r"\Final_models" + r"/model_M={}_{}_Tau={}_SNR={}_T={}".format(M, mode, tau, SNR, T)
+        model = Deep_Root_Net_AntiRectifier(tau=tau, ActivationVal=0.5)  
+        # model = Deep_Root_Net(tau=tau, ActivationVal=0.5)                                         
+        
+        # Load it to the specified device, either gpu or cpu
+        model = model.to(device)         
+        if torch.cuda.is_available() == False:
+                        model.load_state_dict(torch.load(loading_path, map_location=torch.device('cpu')))
+        
+        Losses = PlotSpectrum(model, DataSet_Rx_test, DataSet_x_test, Sys_Model)
+        RootMUSIC_loss.append(Losses[0])
+        MUSIC_loss.append(Losses[1])
+        SPS_RootMUSIC_loss.append(Losses[2])
+        SPS_MUSIC_loss.append(Losses[3])
+        DeepRootTest_loss.append(Losses[4])
             
         print("MUSIC_{} = np.array(".format(mode), MUSIC_loss, ")")
         print("RootMUSIC_{} = np.array(".format(mode) , RootMUSIC_loss, ")")
