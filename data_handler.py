2098666dca4bf1cf417a1ff7e87f8882b8d2a805
Dor Haim Shmuel <shmueldo@post.bgu.ac.il>
2023-05-27 18:32:42 +0300

    data_handler - updated and documented

--- data_handler.py
+++ data_handler.py
@@ -14,33 +14,32 @@ def create_dataset(scenario: str, mode: str, N: int, M: int, T: int,
                     Save:bool=False, dataset_path: str= None,
                     true_doa:list = None, SNR:float = 10, eta: float = 0,
                     geo_noise_var: float = 0, phase:str = None):
-    """function for generating synthetic dataset,
-        each is applied for the desired model_type input.
+    """ Generates a synthetic dataset based on the specified parameters and model type.
 
     Args:
     -----
-    scenario (str): "NarrowBand" or "BroadBand" signals.
-    mode (str): "coherent" or "non-coherent" nature of signals.
-    N (int): number of array sensors.
-    M (int): number of sources.
-    T (int): number of snapshots.
-    samples_size (int): dataset size.
-    tau (int): amount of lags for auto-correlation, relevant only for SubspaceNet model..
-    model_type (str): the model type.
-    Save (bool, optional): wether or not save dataset. Defaults to False.
-    dataset_path (str, optional): path for saving dataset. Defaults to None.
-    true_doa (list, optional): predefined angels. Defaults to None.
-    SNR (float, optional): SNR for samples creations. Defaults to 10.
-    eta (float, optional): sensors distance deviation from ideal array,
-                    relevant for array-mismatches scenarios. Defaults to 0.
-    geo_noise_var (float, optional): steering vector added noise,
-                    relevant for array-mismatches scenarios. Defaults to 0.
-    phase (str, optional): test or training phase for dataset,
-                    relevant only for CNN model. Defaults to None.
+        scenario (str): "NarrowBand" or "BroadBand" signals.
+        mode (str): "coherent" or "non-coherent" nature of signals.
+        N (int): number of array sensors.
+        M (int): number of sources.
+        T (int): number of snapshots.
+        samples_size (int): dataset size.
+        tau (int): amount of lags for auto-correlation, relevant only for SubspaceNet model.
+        model_type (str): the model type.
+        Save (bool, optional): wether or not save dataset. Defaults to False.
+        dataset_path (str, optional): path for saving dataset. Defaults to None.
+        true_doa (list, optional): predefined angels. Defaults to None.
+        SNR (float, optional): SNR for samples creations. Defaults to 10.
+        eta (float, optional): sensors distance deviation from ideal array,
+                                relevant for array-mismatches scenarios. Defaults to 0.
+        geo_noise_var (float, optional): steering vector added noise,
+                                relevant for array-mismatches scenarios. Defaults to 0.
+        phase (str, optional): test or training phase for dataset,
+                                relevant only for CNN model. Defaults to None.
 
     Returns:
     --------
-    tuple: the desired dataset comprised from (X-samples, Y-labels)
+        tuple: the desired dataset comprised from (X-samples, Y-labels)
     """ 
     generic_dataset = []
     model_dataset = []
@@ -82,7 +81,7 @@ def create_dataset(scenario: str, mode: str, N: int, M: int, T: int,
                             geo_noise_var = geo_noise_var)[0], dtype=torch.complex64) 
             if model_type.startswith("SubspaceNet"):
                 # Generate auto-correlation tensor                                   
-                X_model = create_autocorr(X, tau).to(torch.float)
+                X_model = create_autocorrelation_tensor(X, tau).to(torch.float)
             elif model_type.startswith("CNN") and phase.startswith("test"):
                 # Generate 3d covariance parameters tensor
                 X_model = create_cov_tensor(X)
@@ -100,22 +99,46 @@ def create_dataset(scenario: str, mode: str, N: int, M: int, T: int,
     
     return model_dataset, generic_dataset, system_model
 
-def Read_Data(Data_path):
+# def Read_Data(Data_path: str) -> torch.Tensor:
+def Read_Data(Data_path: str):
+    """
+    Reads data from a file specified by the given path.
+
+    Args:
+    -----
+        Data_path (str): The path to the data file.
+
+    Returns:
+    --------
+        torch.Tensor: The loaded data.
+
+    Raises:
+    -------
+        None
+
+    Examples:
+    ---------
+        >>> data_path = "data.pt"
+        >>> Read_Data(data_path)
+
+    """
     Data = torch.load(Data_path)
     return Data
 
-def autocorr_matrix(X, lag):
+# def autocorrelation_matrix(X: torch.Tensor, lag: int) -> torch.Tensor:
+def autocorrelation_matrix(X: torch.Tensor, lag: int):
     '''
-    This function compute the autocorrelation matrices of the T samples
-    x(t), t=1, ... ,T for a given lag.
-    ------- Input ------
-    X(input): Samples matrix input shape [N, T] 
-    lag(input) = the requested delay of the Autocorrelation calculation
-    Rx_lag = the autocorrelation matrix in a given lag []
-    
+    Computes the autocorrelation matrix for a given lag of the input samples.
+
+    Args:
+    -----
+        X (torch.Tensor): Samples matrix input with shape [N, T].
+        lag (int): The requested delay of the autocorrelation calculation.
+
     Returns:
     --------
-    
+        torch.Tensor: The autocorrelation matrix for the given lag.
+
     '''
     Rx_lag = torch.zeros(X.shape[0], X.shape[0], dtype=torch.complex128).to(device)
     for t in range(X.shape[1] - lag):
@@ -127,65 +150,50 @@ def autocorr_matrix(X, lag):
     Rx_lag = torch.cat((torch.real(Rx_lag),torch.imag(Rx_lag)), 0)
     return Rx_lag
 
-def create_autocorr(X, tau):
+# def create_autocorrelation_tensor(X: torch.Tensor, tau: int) -> torch.Tensor:
+def create_autocorrelation_tensor(X: torch.Tensor, tau: int):
     '''
-    This function returns a tensor contains all
-    the autocorrelation tau matrices, for lag 0 to tau
-    {R_x(lag)}, lag  = 0, ..., tau
-    
+    Returns a tensor containing all the autocorrelation matrices for lags 0 to tau.
+
     Args:
-    ------
-    X: observation matrix input, from size (BS, N, T)
-    tau: maximal time difference for auto-correlation tensor
-    
+    -----
+        X (torch.Tensor): Observation matrix input with size (BS, N, T).
+        tau (int): Maximal time difference for the autocorrelation tensor.
+
     Returns:
-    --------------------------------------------
-    Rx_autocorr: Tensor contains all the auto-correlation matrices,
-                 from size (Batch size, tau, 2N, N)
+    --------
+        torch.Tensor: Tensor containing all the autocorrelation matrices,
+                    with size (Batch size, tau, 2N, N).
+
+    Raises:
+    -------
+        None
+
     '''
     Rx_tau = []
     for i in range(tau):
-      Rx_tau.append(autocorr_matrix(X, lag = i))
-    Rx_autocorr = torch.stack(Rx_tau, dim = 0)
+      Rx_tau.append(autocorrelation_matrix(X, lag=i))
+    Rx_autocorr = torch.stack(Rx_tau, dim=0)
     return Rx_autocorr
 
-def create_cov_tensor(X):
+# def create_cov_tensor(X: torch.Tensor) -> torch.Tensor:
+def create_cov_tensor(X: torch.Tensor):
     '''
-    Creating a 3D tensor (NxNx3) contain:
-    1. Rx real part
-    2. Rx imaginary part
-    3. Rx phase component    
+    Creates a 3D tensor of size (NxNx3) containing the real part, imaginary part, and phase component of the covariance matrix.
+
     Args:
     -----
-    X: observation matrix input, size: (N, T)
-    
+        X (torch.Tensor): Observation matrix input with size (N, T).
+
     Returns:
     --------
-    Rx_tensor: Tensor contains all the auto-correlation matrices,
-                 from size (Batch size, N, N, 3)
+        Rx_tensor (torch.Tensor): Tensor containing the auto-correlation matrices, with size (Batch size, N, N, 3).
+
+    Raises:
+    -------
+        None
+
     '''
     Rx = torch.cov(X)
     Rx_tensor = torch.stack((torch.real(Rx),torch.imag(Rx), torch.angle(Rx)), 2)
     return Rx_tensor
-
-if __name__ == "__main__":
-    tau = 8                     # Number of lags
-    N = 8                       # Number of sensors
-    M = 2                       # number of sources
-    T = 100                       # Number of observations, ideal = 200 or above
-    SNR = 10                   # Signal to noise ratio, ideal = 10 or above
-    
-    ## Signal parameters
-    scenario = "NarrowBand"     # signals type, options: "NarrowBand", "Broadband_OFDM", "Broadband_simple"
-    mode = "coherent"           # signals nature, options: "non-coherent", "coherent"
-    
-    ## Array mis-calibration values
-    eta = 0                     # Deviation from sensor location, normalized by wavelength, ideal = 0
-    geo_noise_var = 0           # Added noise for sensors response
-    
-    # simulation parameters
-    samples_size = 70000        # Overall dateset size 
-    train_test_ratio = 0.05    # training and testing datasets ratio 
-    create_dataset_for_cnn(scenario, mode, N, M, T, 1, tau,
-                          Save=False, dataset_path= None, true_doa = 0,
-                          SNR = 10, eta = 0, geo_noise_var = 0)
